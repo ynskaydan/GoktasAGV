9bcf5f9a302d59b30ec7ec72be75da2316379908
ouzzkp <oguzkaan3096@gmail.com>
2023-03-11 15:08:00 +0300

    v1

--- GUI/lib/main.dart
+++ GUI/lib/main.dart
@@ -1,16 +1,19 @@
+// @dart=2.9
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:goktasgui/MqttClientManager.dart';
-
+import 'package:goktasgui/components/controller.dart';
+import 'package:goktasgui/components/mapping.dart';
+import 'package:universal_mqtt_client/universal_mqtt_client.dart';
 import 'package:mqtt_client/mqtt_client.dart';
+import 'package:goktasgui/components/constants.dart';
 
-
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,185 +23,158 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.blueGrey,
         primarySwatch: Colors.lime,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
- MQTTClientManager mqttClientManager = MQTTClientManager();
- 
+  String _timeString;
   @override
   void initState() {
-    setupMqttClient();
-    //setupUpdatesListener();
+    _timeString = _formatDateTime(DateTime.now());
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
     super.initState();
   }
 
-   void _moveForward() {
+  void _getTime() {
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = _formatDateTime(now);
     setState(() {
-      x++;
-      mqttClientManager.publishMessage(
-          pubTopic, "posy: ${y.toString()} move forward");
+      _timeString = formattedDateTime;
     });
-  }
-    void _moveBackward() {
-    setState(() {
-      y--;
-      mqttClientManager.publishMessage(
-          pubTopic, "posy: ${y.toString()} move backward");
-    });
-  }
-    void _moveRight() {
-    setState(() {
-      x++;
-      mqttClientManager.publishMessage(
-          pubTopic, "posx: ${x.toString()} move right");
-    });
-  }
-    void _moveLeft() {
-    setState(() {
-      x--;
-      mqttClientManager.publishMessage(
-          pubTopic, "posx: ${x.toString()} move left");
-    });
-  }
-  final String pubTopic = "yunus";
-  int x = 0;
-  int y = 0;
-  String text = "No Sended Message";
-
-
-  void _sender(int index) {
-    setState(() {
-      switch (index) {
-        case 0:
-          _moveForward();
-          break;
-        case 1:
-          _moveBackward();
-          break;
-        case 2:
-          _moveLeft();
-          break;
-        case 3:
-          _moveRight();
-          break;
-      }
-    });
-  }
-    Future<void> setupMqttClient() async {
-    await mqttClientManager.connect();
-    //mqttClientManager.subscribe(pubTopic);
-  }
-/*
-  void setupUpdatesListener() {
-    mqttClientManager
-        .getMessagesStream()!
-        .listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
-      final recMess = c![0].payload as MqttPublishMessage;
-      final pt =
-          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
-      print('MQTTClient::Message received on topic: <${c[0].topic}> is $pt\n');
-    });
-  }*/
-
-  @override
-  void dispose() {
-    mqttClientManager.disconnect();
-    super.dispose();
   }
 
-
+  String _formatDateTime(DateTime dateTime) {
+    return "${dateTime.hour % 12}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')} ${dateTime.hour < 12 ? '' : ''}";
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.cover),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "GOKTAS AGV CONTROL PANEL",
-              style: TextStyle(color: Colors.lime, fontSize: 50),
-              textAlign: TextAlign.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/goktas.png",
+                  height: MediaQuery.of(context).size.height / 8,
+                  width: MediaQuery.of(context).size.width / 12,
+                )
+              ],
             ),
-            const SizedBox(
-              height: 20,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            DataComponent(
+                              contentData: DataComponentContent(text: "2 m/s"),
+                              subTitle: "Hız",
+                              widthSize: MediaQuery.of(context).size.width / 6,
+                              heightSize:
+                                  MediaQuery.of(context).size.height / 6,
+                            ),
+                            DataComponent(
+                              contentData: DataComponentContent(text: "%87"),
+                              subTitle: "Batarya",
+                              widthSize: MediaQuery.of(context).size.width / 6,
+                              heightSize:
+                                  MediaQuery.of(context).size.height / 6,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            DataComponent(
+                              contentData: DataComponentContent(text: "55 Kg"),
+                              subTitle: "Yük Bilgileri",
+                              widthSize: MediaQuery.of(context).size.width / 6,
+                              heightSize:
+                                  MediaQuery.of(context).size.height / 6,
+                            ),
+                            DataComponent(
+                              contentData: DataComponentContent(text: "25 C"),
+                              subTitle: "Sıcaklık",
+                              widthSize: MediaQuery.of(context).size.width / 6,
+                              heightSize:
+                                  MediaQuery.of(context).size.height / 6,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            DataComponent(
+                              subTitle: "Manuel Kontrol",
+                              contentData: const Controller(),
+                              widthSize:
+                                  MediaQuery.of(context).size.width / 3 + 20,
+                              heightSize:
+                                  MediaQuery.of(context).size.height / 6,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            DataComponent(
+                              subTitle: "Geçen Süre",
+                              contentData:
+                                  DataComponentContent(text: _timeString),
+                              widthSize:
+                                  MediaQuery.of(context).size.width / 3 + 20,
+                              heightSize:
+                                  MediaQuery.of(context).size.height / 6,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    DataComponent(
+                      subTitle: "Harita",
+                      contentData: MappingWidget(),
+                      widthSize: MediaQuery.of(context).size.width / 3,
+                      heightSize: MediaQuery.of(context).size.height / 2 + 40,
+                    ),
+                    DataComponent(
+                      subTitle: "Araç Durumu",
+                      contentData:
+                          DataComponentContent(text: "Araç İstirahatte"),
+                      widthSize: MediaQuery.of(context).size.width / 3,
+                      heightSize: MediaQuery.of(context).size.height / 6,
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: IconButton(
-                      onPressed: () => {_sender(0)},
-                      icon: const Icon(Icons.arrow_upward),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: IconButton(
-                      onPressed: () => {_sender(1)},
-                      icon: const Icon(Icons.arrow_downward),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: IconButton(
-                      onPressed: () => {_sender(2)},
-                      icon: const Icon(Icons.arrow_back),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: IconButton(
-                      onPressed: () => {_sender(3)},
-                      icon: const Icon(Icons.arrow_forward),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              child: Text("Position: $x,$y",
-                  style: const TextStyle(color: Colors.blue, fontSize: 45)),
-            ),
-    
-        
           ],
         ),
       ),
     );
   }
 }
-
