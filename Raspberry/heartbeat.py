9e7567475c1f64c234b61cd1b9e4fc0596fdd8a1
Oğuz Kaan Pehlivan <83175571+ouzzkp@users.noreply.github.com>
2023-03-11 10:36:23 +0300

    Add files via upload

--- Raspberry/heartbeat.py
+++ Raspberry/heartbeat.py
@@ -3,7 +3,12 @@ from mqtt import connect_mqtt, sendData
 topic = "hearbeat"
 client = connect_mqtt()
 def send_heartbeat():
-    
     while True:
         send_data(client, "heartbeat", heartbeat_topic)
         time.sleep(5)  # 5 saniye beklemeP
+        
+    
+# Kullanım örneği ekte gösterilmiştir.
+#  from heartbeat import send_heartbeat
+#  if __name__ == '__main__':
+#      send_heartbeat()#
