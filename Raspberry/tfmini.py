d9ecdfd54d6f00e7d22c8aa2617a3fdcc0f74c0a
Yunus Kaydan <ynskaydan@gmail.com>
2023-03-17 02:13:57 +0300

    Update tfmini for python3

--- Raspberry/tfmini.py
+++ Raspberry/tfmini.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*
 import serial
-
+import time
 ser = serial.Serial("/dev/ttyS0", 115200)
 
 
 def getTFminiData():
     while True:
+    
         count = ser.in_waiting
         if count > 8:
             recv = ser.read(9)
             ser.reset_input_buffer()
-            if recv[0] == 'Y' and recv[1] == 'Y':  # 0x59 is 'Y'
-                low = int(recv[2].encode('hex'), 16)
-                high = int(recv[3].encode('hex'), 16)
-                distance = low + high * 256
-                print(distance)
+            if recv[0] == 0x59 and recv[1] == 0x59:  # 0x59 is 'Y'
+                distance = recv[2] + recv[3] * 256
+                strength = recv[4] + recv[5] * 256
+                print('(',distance,',',strength, ')')
+                time.sleep(1)
 
 
 if __name__ == '__main__':
