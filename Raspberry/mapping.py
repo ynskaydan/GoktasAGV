fddfc7a51d060849827af00b990722ca5b059bd0
ynskaydan <ynskaydan@gmail.com>
2023-03-16 04:22:59 +0300

    mapping dynamics changed
    direction based mapping is applied,
    each of direction is embedeed in software and behavior of save node methods are updated

--- Raspberry/mapping.py
+++ Raspberry/mapping.py
@@ -2,9 +2,6 @@ import json
 from CrossCuttingConcerns.mqtt import connect_mqtt, send_data, broker
 from CrossCuttingConcerns.sub_mqtt import mqtt_sub
 from Graph import Graph
-import paho.mqtt.client as mqtt
-from qr import QR
-from Raspberry.qr import QR
 
 g = Graph()
 pub_topic = "mapping"
@@ -18,18 +15,42 @@ old_value = "S"
 
 g.add_node(old_value, old_posx, old_posy)
 
+direction = "E"
 
-# client = mqtt.Client()
 
-def callbackForQR(client, userdata, msg):
+def update_position(qr, corner_type, direction):
+    corner_actions = {
+        "UPPER_L1": {"E": ("N", -10, 0), "S": ("W", 0, -10)},
+        "UPPER_L2": {"W": ("N", 10, 0), "S": ("E", 0, -10)},
+        "DOWN_L1": {"N": ("E", 0, 10), "W": ("S", 10, 0)},
+        "DOWN_L2": {"N": ("W", 0, 10), "E": ("S", -10, 0)},
+        "T": {"N": (None, 0, 10), "E": (None, -10, 0), "W": (None, 10, 0)},
+        "DOWN_T": {"S": (None, 0, -10), "E": (None, 10, 0), "W": (None, -10, 0)},
+        "RIGHT_T": {"S": (None, 0, 10), "N": (None, 0, -10), "E": (None, -10, 0)},
+        "LEFT_T": {"S": (None, 0, 10), "N": (None, 0, -10), "W": (None, 10, 0)}
+    }
+
+    if corner_type in corner_actions:
+        action = corner_actions[corner_type].get(direction)
+        if action:
+            direction, dx, dy = action
+            posx, posy = qr.get_pos_x() + dx, qr.get_pos_y() + dy
+            if direction:
+                return posx, posy, direction
+
+    print(f"Failed to read message from topic {sub_corner_topic}")
+
+
+def callback_for_qr(client, userdata, msg):
     message = msg.payload.decode('utf-8')  # dinlenen veriyi anlamlı hale getirmek
     parts = message.split(";")  # QR etiketinin standart halinde pozisyonu ayrıştırmak
     g.add_qr(parts[0], parts[1], parts[2])
 
 
-def callbackForCorner(client, userdata, msg):
+def callback_for_corner(client, userdata, msg):
+    global direction
     message = msg.payload.decode('utf-8')
-
+    corner_type = message
     nodes = list(g.get_nodes())
     node_id = nodes[len(nodes) - 1]  # Listedeki en son node çağırmak
     past_node = g.get_node(node_id)
@@ -37,10 +58,11 @@ def callbackForCorner(client, userdata, msg):
     qr_keys = list(g.get_qr_list())
     lastQr = qr_keys[len(qr_keys) - 1]  # en son eklenen qr çağırmak
     qr = g.get_qr(lastQr)
-    ###############################################
-    posx = qr.get_pos_x() + 10  # deneme
-    posy = qr.get_pos_y()
-    ##################################################
+
+    corner = update_position(qr, corner_type, direction)
+    posx = corner[0]
+    posy = corner[1]
+    direction = corner[2]
 
     new_node = g.add_node(g.num_of_nodes, posx, posy)
     weight = int(new_node.get_pos_x() - past_node.get_pos_x()) + int(new_node.get_pos_y() - past_node.get_pos_y())
@@ -78,4 +100,4 @@ def convert_json(graph):
     return converted_json
 
 
-mqtt_sub(broker, sub_qr_topic, callbackForQR, sub_corner_topic, callbackForCorner)
+mqtt_sub(broker, sub_qr_topic, callback_for_qr, sub_corner_topic, callback_for_corner)
