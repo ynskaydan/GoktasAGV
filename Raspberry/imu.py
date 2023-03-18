a3c4900f434c2391bce0e99fce0245e4a2de871c
ynskaydan <ynskaydan@gmail.com>
2023-03-19 00:00:56 +0300

    update imu code

--- Raspberry/imu.py
+++ Raspberry/imu.py
@@ -1,57 +1,43 @@
 import smbus2
 import time
 
-# I2C address of the sensor
-DEVICE_ADDRESS = 0x68
+# Register addresses
+MPU9250_ADDRESS = 0x68
+MPU9250_PWR_MGMT_1 = 0x6B
+MPU9250_ACCEL_XOUT_H = 0x3B
+MPU9250_GYRO_XOUT_H = 0x43
 
-# Register addresses for sensor data
-ACCEL_XOUT_H = 0x3B
-ACCEL_YOUT_H = 0x3D
-ACCEL_ZOUT_H = 0x3F
-TEMP_OUT_H = 0x41
-GYRO_XOUT_H = 0x43
-GYRO_YOUT_H = 0x45
-GYRO_ZOUT_H = 0x47
-COMPASS_XOUT_H = 0x49
-COMPASS_YOUT_H = 0x4B
-COMPASS_ZOUT_H = 0x4D
+AK8963_ADDRESS = 0x0C
+AK8963_CNTL1 = 0x0A
+AK8963_HXL = 0x03
 
-# Initialize I2C bus
-bus = smbus2.SMBus(1)
+bus = smbus2.SMBus(1)  # Use i2c bus 1
 
-# Set up the sensor
-bus.write_byte_data(DEVICE_ADDRESS, 0x6B, 0)
-bus.write_byte_data(DEVICE_ADDRESS, 0x1A, 1 << 2)
-bus.write_byte_data(DEVICE_ADDRESS, 0x1B, 1 << 1)
+# Configure MPU9250
+bus.write_byte_data(MPU9250_ADDRESS, MPU9250_PWR_MGMT_1, 0x00)
 
-# Main loop
 while True:
-    # Read raw sensor data
-    accel_x = read_word_2c(bus, ACCEL_XOUT_H)
-    accel_y = read_word_2c(bus, ACCEL_YOUT_H)
-    accel_z = read_word_2c(bus, ACCEL_ZOUT_H)
-    temp = read_word_2c(bus, TEMP_OUT_H) / 340.0 + 36.53
-    gyro_x = read_word_2c(bus, GYRO_XOUT_H)
-    gyro_y = read_word_2c(bus, GYRO_YOUT_H)
-    gyro_z = read_word_2c(bus, GYRO_ZOUT_H)
-    compass_x = read_word_2c(bus, COMPASS_XOUT_H)
-    compass_y = read_word_2c(bus, COMPASS_YOUT_H)
-    compass_z = read_word_2c(bus, COMPASS_ZOUT_H)
+    # Read accelerometer data
+    accel_x_h = bus.read_byte_data(MPU9250_ADDRESS, MPU9250_ACCEL_XOUT_H)
+    accel_x_l = bus.read_byte_data(MPU9250_ADDRESS, MPU9250_ACCEL_XOUT_H + 1)
+    accel_x = (accel_x_h << 8) | accel_x_l
+    accel_x = accel_x / 16384.0
 
-    # Print sensor data
-    print("Acceleration (m/s^2): ({:.2f}, {:.2f}, {:.2f})".format(accel_x/16384.0, accel_y/16384.0, accel_z/16384.0))
-    print("Temperature: {:.2f} C".format(temp))
-    print("Angular velocity (dps): ({:.2f}, {:.2f}, {:.2f})".format(gyro_x/131.0, gyro_y/131.0, gyro_z/131.0))
-    print("Magnetic field (uT): ({:.2f}, {:.2f}, {:.2f})".format(compass_x*0.92, compass_y*0.92, compass_z*0.92))
+    # Read gyroscope data
+    gyro_x_h = bus.read_byte_data(MPU9250_ADDRESS, MPU9250_GYRO_XOUT_H)
+    gyro_x_l = bus.read_byte_data(MPU9250_ADDRESS, MPU9250_GYRO_XOUT_H + 1)
+    gyro_x = (gyro_x_h << 8) | gyro_x_l
+    gyro_x = gyro_x / 131.0
 
-    # Delay before next reading
-    time.sleep(0.5)
+    # Read magnetometer data
+    bus.write_byte_data(AK8963_ADDRESS, AK8963_CNTL1, 0x16)  # Enter continuous measurement mode
+    time.sleep(0.1)  # Wait for measurement to stabilize
+    mag_x_l = bus.read_byte_data(AK8963_ADDRESS, AK8963_HXL)
+    mag_x_h = bus.read_byte_data(AK8963_ADDRESS, AK8963_HXL + 1)
+    mag_x = (mag_x_h << 8) | mag_x_l
 
-def read_word_2c(bus, reg):
-    high = bus.read_byte_data(DEVICE_ADDRESS, reg)
-    low = bus.read_byte_data(DEVICE_ADDRESS, reg+1)
-    value = (high << 8) + low
-    if (value >= 0x8000):
-        return -((65535 - value) + 1)
-    else:
-        return value
+    print("Accelerometer X: %.2f" % accel_x)
+    print("Gyroscope X: %.2f" % gyro_x)
+    print("Magnetometer X: %d" % mag_x)
+
+    time.sleep(0.1)  # Wait for next reading
