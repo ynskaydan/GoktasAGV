6b778b97af07e2aa9e4aee13c6fd8e7ea32f990a
ynskaydan <ynskaydan@gmail.com>
2023-03-24 04:48:33 +0300

    critical updates for imu

--- Raspberry/Sensors/imu.py
+++ Raspberry/Sensors/imu.py
@@ -1,62 +1,89 @@
 import smbus
+import math
 import time
 
-# initialize the I2C bus
 bus = smbus.SMBus(1)
+address = 0x68
 
-# set the address of the IMU sensor
-address = 0x1e
+# Pusula kalibrasyon sabitleri
+x_offset = 0
+y_offset = 0
+z_offset = 0
+x_scale = 1
+y_scale = 1
 
-# write to the configuration registers of the magnetometer
-bus.write_byte_data(address, 0x00, 0x18)
-bus.write_byte_data(address, 0x01, 0x20)
-bus.write_byte_data(address, 0x02, 0x00)
+def read_byte(address, adr):
+    return bus.read_byte_data(address, adr)
 
-# read the accelerometer data from the IMU sensor
-def read_accelerometer():
-    # set the address of the accelerometer
-    address = 0x1d
+def read_word(address, adr):
+    high = bus.read_byte_data(address, adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
 
-    # read the raw accelerometer data from the sensor
-    x = bus.read_byte_data(address, 0x28)
-    y = bus.read_byte_data(address, 0x2a)
-    z = bus.read_byte_data(address, 0x2c)
+def read_word_2c(address, adr):
+    val = read_word(address, adr)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
 
-    # convert the raw accelerometer data to G units
-    x = (x / 63.0) - 1.0
-    y = (y / 63.0) - 1.0
-    z = (z / 63.0) - 1.0
+def write_byte(address, adr, value):
+    bus.write_byte_data(address, adr, value)
 
-    # return the accelerometer data as a tuple
-    return (x, y, z)
+def calibrate_compass():
+    global x_offset, y_offset, z_offset, x_scale, y_scale
+    x_offset = -84.5
+    y_offset = -146.5
+    z_offset = -126.5
+    x_scale = 1.16
+    y_scale = 1.08
 
-# read and calibrate the magnetometer data from the IMU sensor
-def read_magnetometer():
-    # set the address of the magnetometer
-    address = 0x1e
+def setup_imu():
+    # MPU6050 için güç yönetimini ayarlama
+    write_byte(address, 0x6B, 0x00)
 
-    # read the raw magnetometer data from the sensor
-    x = bus.read_byte_data(address, 0x03) << 8 | bus.read_byte_data(address, 0x04)
-    z = bus.read_byte_data(address, 0x05) << 8 | bus.read_byte_data(address, 0x06)
-    y = bus.read_byte_data(address, 0x07) << 8 | bus.read_byte_data(address, 0x08)
+    # AK8975C için ölçek faktörünü ayarlama
+    write_byte(0x0C, 0x0A, 0x16)
 
-    # calibrate the magnetometer data
-    x = (x - 152.0) / 315.0
-    y = (y - 81.0) / 335.0
-    z = (z - 110.0) / 250.0
+def read_imu():
+    # Pusula verileri okuma
+    x_out = read_word_2c(address, 0x03) - x_offset
+    y_out = read_word_2c(address, 0x05) - y_offset
+    z_out = read_word_2c(address, 0x07) - z_offset
 
-    # return the magnetometer data as a tuple
-    return (x, y, z)
+    # Pusula verilerinin yönlerini belirleme
+    bearing = math.atan2(y_out * y_scale, x_out * x_scale)
+    if (bearing < 0):
+        bearing += 2 * math.pi
 
-# continuously read and display the accelerometer and magnetometer data
+    # İvme verileri okuma
+    x_acc_out = read_word_2c(address, 0x3B)
+    y_acc_out = read_word_2c(address, 0x3D)
+    z_acc_out = read_word_2c(address, 0x3F)
+
+    # İvme verilerini hesaplama
+    x_acc_scaled = x_acc_out / 16384.0
+    y_acc_scaled = y_acc_out / 16384.0
+    z_acc_scaled = z_acc_out / 16384.0
+
+    # Sonuçları döndürme
+    return (math.degrees(bearing), x_acc_scaled, y_acc_scaled, z_acc_scaled)
+
+# Pusula kalibrasyonunu yapma
+calibrate_compass()
+
+# IMU ayarlarını yapma
+setup_imu()
+
 while True:
-    # read the accelerometer and magnetometer data
-    accel_data = read_accelerometer()
-    mag_data = read_magnetometer()
+    # Verileri okuma
+    bearing, x_acc, y_acc, z_acc = read_imu()
 
-    # print the accelerometer and magnetometer data
-    print("Accelerometer data: ", accel_data)
-    print("Magnetometer data: ", mag_data)
+    # Sonuçları yazdırma
+    print("Pusula Yönü:", bearing)
+    print("X İvme:", x_acc)
+    print("Y İvme:", y_acc)
+    print("Z İvme:", z_acc)
 
-    # wait for 1 second before reading the data again
-    time.sleep(1)
+    # 1
