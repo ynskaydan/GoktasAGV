c854062a76c2f43ac49b71d27e80d89651a31ff9
ynskaydan <ynskaydan@gmail.com>
2023-04-18 03:36:05 +0300

    Merge remote-tracking branch 'origin/main' into main

---
+++ Raspberry/path.py
@@ -0,0 +1,25 @@
+import mapping
+class Path:
+    def __init__(self):
+        self.is_path_following = False
+        self.target_node 
+        
+    
+    
+def findPath(graph, startNode, endNode):
+    
+    # path finding algorithm: djikstra or some graph traversal
+    return [startNode, intermediateNode1, intermediateNode2, endNode]
+
+def startFollowPath(graph, path):
+    mapping.setPathFollowingFlag(True)
+    direction = findRequiredDirectionFor(path[0], path[1])
+    turnTo(direction)
+    startAutonomousMotionOfVehicle()
+    
+
+
+
+
+    
+        
